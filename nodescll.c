726d60be9fd2a9a0d379318d2de6e799ba9492b3
AdityaThundersoft <aditya.v@thundersoft.com>
2024-04-25 00:57:02 +0530

    circular linkedlists

---
+++ nodescll.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node* newcll(int n)
+{
+    struct node *temp = (struct node *) malloc(sizeof(struct node));
+    temp->data = n;
+    temp->next = temp;
+    return temp;
+}
+
+struct node* addend(struct node *tail, int n)
+{
+    struct node *temp = (struct node *) malloc(sizeof(struct node));
+    temp->data = n;
+    temp->next = tail->next;
+    tail->next = temp;
+    return temp;
+}
+
+struct node* addbeg(struct node *tail, int n)
+{
+    struct node *temp = (struct node *) malloc(sizeof(struct node));
+    temp->data = n;
+    temp->next = tail->next;
+    tail->next = temp;
+    return tail;
+}
+
+struct node* addmid(struct node *tail, int n, int pos)
+{
+    struct node *temp = (struct node *) malloc(sizeof(struct node));
+    struct node *ptr;
+    ptr = tail->next;
+    while(--pos)
+    {
+        ptr = ptr->next;
+    }
+    if(ptr == tail)
+    {
+        tail = addend(tail,n);
+        return tail;
+    }
+    temp->data = n;
+    temp->next = ptr->next;
+    ptr->next = temp;
+    return tail;
+}
+
+struct node* del(struct node* tail, int pos)
+{
+    struct node *temp = (struct node *) malloc(sizeof(struct node));
+    temp = tail->next;
+    if(pos == 1)
+    {
+        tail->next = temp->next;
+        free(temp);
+        return tail;
+    }
+    pos = pos-1;
+    while(--pos)
+    {
+        temp = temp->next;
+    }
+    temp->next = temp->next->next;
+    temp = temp->next;
+    free(temp);
+    return tail;
+}
+
+int nodes(struct node *tail)
+{
+    struct node *ptr;
+    int count = 0;
+    ptr = tail->next;
+    do
+    {
+        count++;
+        ptr = ptr->next;
+    }while(ptr!=tail->next);
+    return count;
+}
+
+void print(struct node *tail)
+{
+    struct node *ptr;
+    ptr = tail->next;
+    do
+    {
+        printf("%d ",ptr->data);
+        ptr = ptr->next;
+    }while (ptr!=tail->next);
+    
+}
+
+int main()
+{
+    struct node *tail = (struct node *) malloc(sizeof(struct node));
+    tail = newcll(15);
+    tail = addend(tail,10);
+    tail = addend(tail,20);
+    tail = addend(tail,30);
+    printf("%d\n",nodes(tail));
+    tail = addbeg(tail,15);
+    tail = addmid(tail,64,2);
+    printf("%d\n",nodes(tail));
+    tail = del(tail,1);
+    printf("%d\n",nodes(tail));
+
+    
+    return 0;
+} 
+// 4
+// 6
+// 5
